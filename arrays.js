6c96adacf908b41f6c6cc1fa1c75bc4f493283de
Matthew Migliore <mmigliore311@gmail.com>
2018-08-16 23:49:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(a,b) {
-  
+function addElementToBeginningOfArray(array,element) {
+ newArray = array.unshift(element)
+ return newArray
 }
